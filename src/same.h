6208cf2c3104a8236c75e2747146954ce5c35aec
Josh Williams <jodawill@iuk.edu>
2015-03-28 22:55:27 -0400

    Organized same.h

--- src/same.h
+++ src/same.h
@@ -5,56 +5,79 @@
 #include <time.h>
 #include <sys/stat.h>
 
-int width;
-int height;
-int max_width, max_height;
-int board[1024][1024];
-int board_undo[1024][1024][1024];
-static int blocknum = 4;
-char hsn[14];
-
-// hst.c
-int set_hst_fn();
-
+// Global variables
 bool gameover;
-int set_width(int w);
-int set_height(int h);
+int x,y;
 int display_height, display_width;
 char hst_fn[1024];
 bool easy;
 bool god;
 int highscore;
 bool hled[1024][1024];
-bool is_game_over();
-bool is_block_alone(int x, int y);
-int draw_logo();
-int get_hsn();
-int read_hst();
-int write_hst();
-int draw_hst();
-int draw_board();
+int width;
+int height;
+int max_width, max_height;
+int board[1024][1024];
+int board_undo[1024][64][64];
+static int blocknum = 4;
+char hsn[14];
 int redonum;
-int draw_undo();
-int end_game();
-int draw_command(const char *text);
-int draw_error(const char *text);
-int clear_command();
-int draw_score();
 int undonum;
 int score_undo[1024];
-int undo();
-int redo();
-int copy_board();
 int n, score;
-int delblock(int x, int y);
-int highlight(int x, int y);
-int highlight_none();
-int init();
-int get_rand(int cap);
-int reset_board();
-int cursor_wait();
-int cleanup();
-int define_colors();
-int x,y;
 static char blocks[16] = " abcdABCD";
+bool hl_board[64][64];
+
+// board.c
+bool is_block_alone(int col, int row);
+bool is_game_over();
+int reset_board();
+int set_height();
+int set_width();
+
+// colors.c
+int define_colors();
+
+// command.c
+int clear_command();
+int command_wait();
+bool confirm(const char* str);
+int cursor_wait();
+int draw_board();
+int draw_command(const char* text);
+int draw_error();
+int draw_hst();
+int draw_logo();
+int draw_score();
+int draw_undo();
+
+// endgame.c
+int end_game();
+
+// highlight.c
+int check_hl(int x,int y,char bl,bool remq);
+int collapse();
+int collapse_horizontal();
+int collapse_vertical();
+int delblock(int x,int y);
+int highlight(int x,int y);
+int highlight_none();
+bool move_horizontal(int col);
+bool is_col_empty(int col);
+
+// hst.c
+int get_hsn();
+int read_hst();
+int set_hst_fn();
+int write_hst();
+
+// init.c
+int cleanup();
+int get_rand(int n);
+int init();
+
+// save.c
+int copy_board();
+int redo();
+int undo();
 
