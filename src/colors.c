a20a3246782a0a8fc2e1b9081e6f3e6926e0e555
Josh Williams <jodawill@iuk.edu>
2015-04-09 20:57:28 -0400

    Don't define colors if ncurses isn't supported

--- src/colors.c
+++ src/colors.c
@@ -1,20 +1,23 @@
 #include "same.h"
 
 int define_colors() {
- init_pair(1,COLOR_BLACK,COLOR_RED);
- init_pair(2,COLOR_BLACK,COLOR_BLUE);
- init_pair(3,COLOR_BLACK,COLOR_GREEN);
- init_pair(4,COLOR_BLACK,COLOR_YELLOW);
+ #if defined(__NCURSES__)
+  init_pair(1,COLOR_BLACK,COLOR_RED);
+  init_pair(2,COLOR_BLACK,COLOR_BLUE);
+  init_pair(3,COLOR_BLACK,COLOR_GREEN);
+  init_pair(4,COLOR_BLACK,COLOR_YELLOW);
 
- init_pair(5,COLOR_WHITE,COLOR_MAGENTA);
- init_pair(6,COLOR_WHITE,COLOR_CYAN);
- init_pair(7,COLOR_WHITE,COLOR_GREEN);
- init_pair(8,COLOR_WHITE,COLOR_YELLOW);
+  init_pair(5,COLOR_WHITE,COLOR_MAGENTA);
+  init_pair(6,COLOR_WHITE,COLOR_CYAN);
+  init_pair(7,COLOR_WHITE,COLOR_GREEN);
+  init_pair(8,COLOR_WHITE,COLOR_YELLOW);
 
- init_pair(20,COLOR_WHITE,COLOR_BLACK);
- init_pair(21,COLOR_WHITE,COLOR_RED);
+  init_pair(20,COLOR_WHITE,COLOR_BLACK);
+  init_pair(21,COLOR_WHITE,COLOR_RED);
 
- init_pair(100,COLOR_BLACK,COLOR_WHITE);
- return 0;
+  init_pair(100,COLOR_BLACK,COLOR_WHITE);
+  return 0;
+ #endif
+ return -1;
 }
 
